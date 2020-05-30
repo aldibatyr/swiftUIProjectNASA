8b398104db165eb95f69d8c81854bfdb5a32295d
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2020-05-29 18:48:42 -0700

    initial commit

---
+++ moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moonshot
+//
+//  Created by Aldiyar B on 5/29/20.
+//  Copyright © 2020 Aldiyar B. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
